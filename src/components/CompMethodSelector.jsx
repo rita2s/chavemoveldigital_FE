cc992e9d0feba015db836b169e0191f40c1702cf
rita_santos <rassa2@iscte-iul.pt>
2025-06-26 20:26:35 +0100

    feat: enhance CompMethodSelector with authentication method buttons

--- src/components/CompMethodSelector.jsx
+++ src/components/CompMethodSelector.jsx
@@ -1,8 +1,19 @@
+import CompRoundButton from "./CompRoundButton.jsx";
 
 const CompMethodSelector = ({selected, onSelect}) => {
     return (
         <div className="comp-method">
-            <h4>SELECIONE O MÉTODO DE AUTENTICAÇÃO</h4>
+            <h4>SELECIONE O MÉTODO DE AUTENTICAÇÃO:</h4>
+            <CompRoundButton
+                label="Cartão de Cidadão"
+                selected={selected === 'cc'}
+                onClick={() => onSelect('cc')}
+                />
+            <CompRoundButton
+                label="Chave Móvel Digital"
+                selected={selected === 'cmd'}
+                onClick={() => onSelect('cmd')}
+                />
         </div>
     )
 }

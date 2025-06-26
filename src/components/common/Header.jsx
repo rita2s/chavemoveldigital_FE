37c15ebe78df7bb97f474aa90ff9ed91d38a3ce8
Alexandre Ardigo <alexaardigo@gmail.com>
2025-06-26 21:43:57 +0100

    style: enhance Header component with custom styles for container and image

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const Header = () => {
-    return (
-        <div>
+    const style = {
+        container: {
+            width: "100%",
+            backgroundColor: "var(--blue-autGov)",
+            display: "inline-flex",
+            justifyContent: "center"
 
+        },
+        img: {
+            width: "200px",
+        }
+    }
+    return (
+        <div style={style.container}>
+            <img style={style.img} src="public/images/image-autGov.png" alt=""/>
         </div>
     );
 };

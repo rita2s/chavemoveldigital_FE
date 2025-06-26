18f3fb41b0af2e7afa6d270dc3e852da9bc09491
rita_santos <rassa2@iscte-iul.pt>
2025-06-26 20:25:13 +0100

    feat: update CompMethodSelector to include authentication method selection UI

--- src/components/CompMethodSelector.jsx
+++ src/components/CompMethodSelector.jsx
@@ -1,4 +1,8 @@
 
 const CompMethodSelector = ({selected, onSelect}) => {
-    
+    return (
+        <div className="comp-method">
+            <h4>SELECIONE O MÉTODO DE AUTENTICAÇÃO</h4>
+        </div>
+    )
 }

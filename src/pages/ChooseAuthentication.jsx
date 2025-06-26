a0f103831ade26699c2464009d24f930862fe208
rita_santos <rassa2@iscte-iul.pt>
2025-06-26 21:41:23 +0100

    feat: clean up ChooseAuthentication component by removing unused imports and whitespace

--- src/pages/ChooseAuthentication.jsx
+++ src/pages/ChooseAuthentication.jsx
@@ -1,13 +1,11 @@
-import CompRoundButton from "../components/CompRoundButton.jsx";
-import CompMethodSelector from "../components/CompMethodSelector.jsx";
-import CompBarProgress from "../components/CompBarProgress.jsx";
+
 import CompAutMethod from "../components/CompAutMethod.jsx";
 
 const ChooseAuthentication = () => {
     return (
         <div>
             <CompAutMethod/>
-            
+
         </div>
     )
 }

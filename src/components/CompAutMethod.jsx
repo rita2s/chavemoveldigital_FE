128818124a56469d145dda0869dc766787e9b467
rita_santos <rassa2@iscte-iul.pt>
2025-06-26 19:50:35 +0100

    feat: implement initial structure for CompAutMethod component

--- src/components/CompAutMethod.jsx
+++ src/components/CompAutMethod.jsx
@@ -1,4 +1,16 @@
 
 const CompAutMethod = () => {
 
-}
+    return (
+        <div className="comp-aut-method">
+            <h3>FAÇA A SUA AUTENTICAÇÃO COM:</h3>
+            <CompProgressBar progress/>
+            <CompAutMethod
+                selected={selectedMethod}
+                onSelect={setSelectedMethod}
+            />
+        </div>
+    );
+};
+
+export default CompAutMethod;

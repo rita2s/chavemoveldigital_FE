1a90765971cfa53e9e756ff9b059c4a2d7208cae
rita_santos <rassa2@iscte-iul.pt>
2025-06-26 23:49:10 +0100

    feat: add additional authentication method options in CompMethodSelector component

--- src/components/CompMethodSelector.jsx
+++ src/components/CompMethodSelector.jsx
@@ -2,6 +2,7 @@ import CompRoundButton from "./CompRoundButton.jsx";
 
 const CompMethodSelector = ({selected, onSelect}) => {
     return (
+        <div>
         <div className="comp-method">
             <h4>SELECIONE O MÉTODO DE AUTENTICAÇÃO:</h4>
             <CompRoundButton
@@ -14,6 +15,20 @@ const CompMethodSelector = ({selected, onSelect}) => {
                 selected={selected === 'cmd'}
                 onClick={() => onSelect('cmd')}
                 />
+        </div>
+            <div className="comp-method">
+                <h4>Pretende fazer a autenticação através de:</h4>
+                <CompRoundButton
+                    label="Email"
+                    selected={selected === 'email'}
+                    onClick={() => onSelect('email')}
+                />
+                <CompRoundButton
+                    label="Número de Telemóvel"
+                    selected={selected === 'sms'}
+                    onClick={() => onSelect('sms')}
+                />
+            </div>
         </div>
     )
 }

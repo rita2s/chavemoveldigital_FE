954352fdf2b1e0a0d19502ce9adc0b0f95c7d794
rita_santos <rassa2@iscte-iul.pt>
2025-06-27 18:09:07 +0100

    feat: add ProgressBar component to CompAutMethod for visual feedback

--- src/components/CompAutMethod.jsx
+++ src/components/CompAutMethod.jsx
@@ -1,11 +1,13 @@
 import  React from 'react';
 import CompMethodSelector from "./CompMethodSelector.jsx";
+import ProgressBar from "./common/ProgressBar/index.jsx";
 
 const CompAutMethod = () => {
 
     return (
         <div className="comp-aut-method">
             <h3>FAÇA A SUA AUTENTICAÇÃO COM:</h3>
+            <ProgressBar progress={30}/>
             <CompMethodSelector />
         </div>
     );

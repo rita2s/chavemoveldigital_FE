3375d7d3d6b00599cd33c1fe5b6ccf907b4d38ea
Alexandre Ardigo <alexaardigo@gmail.com>
2025-06-27 12:21:26 +0100

    add axios instance for API communication

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+    // URL base da nossa API Spring Boot
+    baseURL: 'http://localhost:9090',
+    // Envia os cookies de sessão em cada requisição
+    withCredentials: true
+});
+
+export default api;

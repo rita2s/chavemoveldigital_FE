609c08b20668d002355161b0ff01eba10ccace3c
rita_santos <rassa2@iscte-iul.pt>
2025-06-27 18:05:17 +0100

    refactor: remove local state management from CompAutMethod and simplify CompMethodSelector usage

--- src/components/CompAutMethod.jsx
+++ src/components/CompAutMethod.jsx
@@ -1,18 +1,14 @@
-import  React, { useState } from 'react';
+import  React from 'react';
 import CompBarProgress from "./CompBarProgress.jsx";
 import CompMethodSelector from "./CompMethodSelector.jsx";
 
 const CompAutMethod = () => {
-    const [selectMethod, setSelectedMethod] = useState(null);
 
     return (
         <div className="comp-aut-method">
             <h3>FAÇA A SUA AUTENTICAÇÃO COM:</h3>
             <CompBarProgress progress/>
-            <CompMethodSelector
-                selected={selectMethod}
-                setSelectedMethod={setSelectedMethod}
-            />
+            <CompMethodSelector />
         </div>
     );
 };

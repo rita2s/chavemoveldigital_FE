3a0470bc21a715146f450ad1c35d921084bf6d00
rita_santos <rassa2@iscte-iul.pt>
2025-06-28 15:32:52 +0100

    feat: update CompAgreeToData component with authentication method selection and information

--- src/components/CompAgreeToData.jsx
+++ src/components/CompAgreeToData.jsx
@@ -2,9 +2,26 @@
 const CompAgreeToData = () => {
 
     return (
-        <h3>OLAOLA</h3>
+
+        <div className="comp-method">
+            <h4>SELECIONE O MÉTODO DE AUTENTICAÇÃO:</h4>
+            <div className="inline-row">
+                <h5>Autoridade Tributária e Aduaneira</h5>
+                <p>solicitou alguns dos seus dados para realizar o serviço online pretendido</p>
+            </div>
+
+            <h5>Identificação Fiscal</h5>
+
+            <div className="inline-row">
+                <h5>www.autenticacao.gov.pt</h5>
+                <p>solicitou alguns dos seus dados para realizar o serviço online pretendido</p>
+            </div>
+
+            <h5>Nome Próprio<br /><br />Apelido<br /><br />Localidade<br /><br />Identificação Civil</h5>
+        </div>
 
     )
+
 }
 
 export default CompAgreeToData;

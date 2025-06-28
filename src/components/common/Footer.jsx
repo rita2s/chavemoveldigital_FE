7fd7b4515baa7c2bd830cfe256b1074e90517f2d
rita_santos <rassa2@iscte-iul.pt>
2025-06-28 13:20:29 +0100

    style: enhance Footer component with improved layout and styling

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <div>
+    const style = {
+        container: {
+            width: "100%",
+            display: "inline-flex",
+            justifyContent: "center",
+            margin: "20px 0"
 
-        </div>
+        },
+        img: {
+            width: "316px",
+        },
+        disclaimer: {
+            maxWidth: "700px",
+            margin: "90px auto 0 auto",
+            fontSize: "14px",
+            lineHeight: "1.5",
+            textAlign: "center",
+        }
+    };
+
+    return (
+        <>
+            <div style={style.disclaimer}>
+                <p>
+                    O processo de autenticação é um serviço da Identificação Eletrónica que permite confirmar a sua identidade, facilitando o acesso online a vários serviços públicos.  <a href="https://www.gov.pt/pt/servicos/autenticacao-eletronica">Saiba mais sobre a Identificação Eletrónica</a>.
+                </p>
+            </div>
+
+            <div style={style.container}>
+                <img style={style.img} src="public/images/image-ama.png" alt=""/>
+            </div>
+        </>
     );
 };
 

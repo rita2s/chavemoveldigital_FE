9f82bf515354f9ffcb862af2dd264d315a99410d
rita_santos <rassa2@iscte-iul.pt>
2025-06-28 15:45:19 +0100

    feat: update CompAgreeToData component layout and content for authentication method selection

--- src/components/CompAgreeToData.jsx
+++ src/components/CompAgreeToData.jsx
@@ -5,20 +5,15 @@ const CompAgreeToData = () => {
 
         <div className="comp-method">
             <h4>SELECIONE O MÉTODO DE AUTENTICAÇÃO:</h4>
-            <div className="inline-row">
+            <div className="inline-row" >
                 <h5>Autoridade Tributária e Aduaneira</h5>
                 <p>solicitou alguns dos seus dados para realizar o serviço online pretendido</p>
             </div>
 
-            <h5>Identificação Fiscal</h5>
 
-            <div className="inline-row">
-                <h5>www.autenticacao.gov.pt</h5>
-                <p>solicitou alguns dos seus dados para realizar o serviço online pretendido</p>
-            </div>
-
-            <h5>Nome Próprio<br /><br />Apelido<br /><br />Localidade<br /><br />Identificação Civil</h5>
+            <h5>Nome Próprio<br /><br />Apelido<br /><br />Localidade<br /><br />Identificação Civil<br/><br/>Identificação Fiscal</h5>
         </div>
+
 
     )
 

4cfca704fd3b7a7384ea9de9632a419bc882cfd0
rita_santos <rassa2@iscte-iul.pt>
2025-06-28 14:29:21 +0100

    style: update Footer component with new copyright section and improved margin

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             width: "100%",
             display: "inline-flex",
             justifyContent: "center",
-            margin: "20px 0"
+            margin: "30px 0 0 0",
 
         },
         img: {
@@ -18,6 +18,17 @@ const Footer = () => {
             fontSize: "14px",
             lineHeight: "1.5",
             textAlign: "center",
+        },
+        copyright: {
+            maxWidth: "600px",
+            margin: "5px auto 20px auto",
+            fontSize: "12px",
+            lineHeight: "1.5",
+            textAlign: "center",
+            color: "black"
+        },
+        link: {
+            color: "#838383"
         }
     };
 
@@ -25,12 +36,17 @@ const Footer = () => {
         <>
             <div style={style.disclaimer}>
                 <p>
-                    O processo de autenticação é um serviço da Identificação Eletrónica que permite confirmar a sua identidade, facilitando o acesso online a vários serviços públicos.  <a href="https://www.gov.pt/pt/servicos/autenticacao-eletronica">Saiba mais sobre a Identificação Eletrónica</a>.
+                    O processo de autenticação é um serviço da Identificação Eletrónica que permite confirmar a sua <br/> identidade,<br/> facilitando o acesso online a vários serviços públicos.  <a href="https://www.gov.pt/pt/servicos/autenticacao-eletronica">Saiba mais sobre a Identificação Eletrónica</a>.
                 </p>
             </div>
 
             <div style={style.container}>
                 <img style={style.img} src="public/images/image-ama.png" alt=""/>
+            </div>
+
+            <div style={style.copyright}>
+                AMA - Agência para a Modernização Administrativa, IP - Todos os direitos reservados @ 2025<br />
+                <a href="https://www.ama.gov.pt" style={style.link}>www.ama.gov.pt</a> | <a href="mailto:ama@ama.pt" style={style.link}>ama@ama.pt</a>
             </div>
         </>
     );

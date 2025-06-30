ece55f484caff7448091c6736c294a844869b16b
rita_santos <rassa2@iscte-iul.pt>
2025-06-30 20:59:33 +0100

    feat: add button details configuration in Unavailable component

--- src/pages/Unavailable.jsx
+++ src/pages/Unavailable.jsx
@@ -1,12 +1,31 @@
-import CompAutMethod from "../components/CompAutMethod.jsx";
 import ButtonsContainer from "../components/ButtonsContainer.jsx";
 import MainContainer from "../components/MainContainer.jsx";
-import React from "react";
+import React, {useState} from "react";
+
 
 const Unavailable = () => {
     const handleReturn = () => navigate("/authorization");
 
-
+    const buttonsDetails = {
+        className: {
+            return: {
+                container: "dflx g10 pt-sans-bold blue-autGov",
+                button: "btn-grey-main",
+                arrow: "arrow-blue"
+            },
+            advance: {
+                container: "dflx g10 pt-sans-bold white-autGov",
+                button: "btn-blue-main",
+                arrow: "arrow-white"
+            }
+        },
+        style: {
+            h2: {
+                margin: 0,
+            }
+        }
+    };
+    
     return (
         <MainContainer
             className={"dflxc g20"}
